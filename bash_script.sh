2e567b91ac110c653e14405e8874394660540438
jgspedreira <jgspedreira@usp.br>
2024-10-10 11:11:13 -0300

    Add bash_script.sh and lista_HOME.txt

---
+++ bash_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Mostrar o diretório atual
+echo "Diretório atual:"
+pwd
+
+# Gerar uma lista detalhada do conteúdo da pasta HOME e salvar em lista_HOME.txt
+ls -l $HOME > lista_HOME.txt
+
+# Imprimir a data atual
+echo "Data atual:"
+date
+
